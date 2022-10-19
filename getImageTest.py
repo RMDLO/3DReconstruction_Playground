882167048de918dbaba2fd9ca758f1adfb9504ef
Fsamhouri <fuad2@illinois.edu>
2022-10-19 14:25:19 -0500

    added image capture functionality into getImageTest script

--- getImageTest.py
+++ getImageTest.py
@@ -23,6 +23,7 @@ config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 
 pipeline.start(config)
 
+img_counter = 0
 try:
     while True:
 
@@ -48,7 +49,17 @@ try:
         # Show images
         cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
         cv2.imshow('RealSense', color_image)
-        cv2.waitKey(1)
+        # ret,frame = color_image.read()
+        k = cv2.waitKey(1)
+
+        if k%256 == 27:
+            print("Escape hit, closing the app")
+            break
+        elif k%256 == 32:
+            img_name = "opencv_frame_{}.png".format(img_counter)
+            cv2.imwrite(img_name, color_image)
+            print("screenshot taken")
+            img_counter += 1
 
 finally:
 
